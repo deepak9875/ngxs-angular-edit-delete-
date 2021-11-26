8965bce89a0ee91701af36fef3feeaeb2602df2e
deepak9875 <deepaklakhlan1234@gmail.com>
2021-11-26 12:32:00 +0530

    text change in app.state.ts file

--- src/app/states/app.state.ts
+++ src/app/states/app.state.ts
@@ -20,7 +20,7 @@ export class UserStateModel {
 
 @Injectable()
 export class AppState {
-    constructor(private _du: DesignutilityService) { }
+    constructor(private thug_life: DesignutilityService) { }
 
 
 
@@ -37,7 +37,7 @@ export class AppState {
     
     @Action(GetUsers)
     getDataFromState(ctx: StateContext<UserStateModel>) {
-        return this._du.fetchUsers().pipe(tap(returnData => {
+        return this.thug_life.fetchUsers().pipe(tap(returnData => {
             const state = ctx.getState();
 
             ctx.setState({
@@ -49,7 +49,7 @@ export class AppState {
 
     @Action(AddUsers)
     addDataToState(ctx: StateContext<UserStateModel>, { payload }: AddUsers) {
-        return this._du.addUsers(payload).pipe(tap(returnData => {
+        return this.thug_life.addUsers(payload).pipe(tap(returnData => {
             const state=ctx.getState();
             ctx.patchState({
                 users:[...state.users,returnData]
@@ -59,7 +59,7 @@ export class AppState {
 
     @Action(UpdateUsers)
     updateDataOfState(ctx: StateContext<UserStateModel>, { payload, id, }: UpdateUsers) {
-        return this._du.updateUser(payload, id).pipe(tap(returnData => {
+        return this.thug_life.updateUser(payload, id).pipe(tap(returnData => {
             const state=ctx.getState();
 
             const userList = [...state.users];
@@ -77,7 +77,7 @@ export class AppState {
 
     @Action(DeleteUsers)
     deleteDataFromState(ctx: StateContext<UserStateModel>, { id }: DeleteUsers) {
-        return this._du.deleteUser(id).pipe(tap(returnData => {
+        return this.thug_life.deleteUser(id).pipe(tap(returnData => {
             const state=ctx.getState();
             console.log("The is is",id)
             //Here we will create a new Array called filteredArray which won't contain the given id and set it equal to state.todo
